6d43b6455f96b39e395ae78c0943a8784c6c18ea
Amit Chaudhari <amitchaudhari@Amits-MacBook.local>
2017-03-01 22:57:37 -0500

    working leap input and shooting

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  context: __dirname,
+  entry: "./lib/entry.js",
+  output: {
+    path: "./lib",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx" ]
+  },
+  devtool: 'source-map'
+};
